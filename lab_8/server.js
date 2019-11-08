223b2692a1904f3088e51babbac4ecd0094340b3
Ayesha Shafquat <ashafqua@terpmail.umd.edu>
2019-11-07 21:25:18 -0500

    final lab 8

--- lab_8/server.js
+++ lab_8/server.js
@@ -10,28 +10,17 @@ app.use(express.static('public'));
 
 app.get('/api', (req, res) => {
   const baseURL = 'https://api.umd.io/v0/courses/list';
-  courses = []
   fetch(baseURL)
     .then(r => r.json())
-    .then(r => r.map(c => c.name))
-    .then(name => {
-      fetch(baseURL)
-        .then(r => r.json())
-        .then(r => r.filter(c => c.dept_id))
-        .then(dept_id => {
-          for(let i=0; i<dept_id.length; i+=1) {
-            if(dept_id[i]==="INST") {
-              courses += dept_id[i] + "\n"
-            }
-          }
-           console.log(courses);
-           res.send({ courses: courses })
+    .then(r => r.filter(c => {if (c.dept_id=="INST")return c.name}))
+    .then((data) => {
+           console.log(data);
+           res.send({ data: data });
+    })
         .catch((err) => {
          console.log(err);
          res.redirect('/error'); 
-        });
-      });
-    });
-});
+        })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
